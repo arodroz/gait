b6d4dd5f4e386c95aeaddfed8a4df05d2ac0dac5
ilres-antonio <159048975+ilres-antonio@users.noreply.github.com>
2026-03-20 21:22:21 +0100

    Revert "test: dummy change for rollback testing"
    This reverts commit 9d862c907efa8c5efc6a7e735ca10ad544f5446c.

--- src/core/util.ts
+++ src/core/util.ts
@@ -25,8 +25,3 @@ export function formatDuration(ms: number): string {
 export function timestamp(): string {
   return new Date().toLocaleTimeString("en-GB", { hour12: false });
 }
-
-/** This is a dummy function for testing rollback */
-export function dummy(): string {
-  return "this should be reverted";
-}
